ea8f8a0e0b017df840b3237648d59f445c3d03db
Camilo Parra <caparra92@gmail.com>
2020-08-03 21:50:23 -0500

    Deleting red background

--- src/components/formContact.vue
+++ src/components/formContact.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex w-full h-auto py-5 justify-center items-center bg-red-100">
+    <div class="flex w-full h-auto py-5 justify-center items-center">
         <form class="flex flex-col bg-white w-full p-5 justify-center items-center">
             <h1 class="text-center">Get in touch</h1>
             <input type="text" class="border-none resize-none outline-none py-3 px-0 text-sm w-5/6 m-2 text-blue-900 transition-all duration-300 border-b focus:border-purple-700" id="name" placeholder="Name">
